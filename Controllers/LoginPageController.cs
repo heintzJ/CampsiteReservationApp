cd1ad013f68c7ad798a9e6f59fdc16469ddbd5c8
Jack Heintz <jheintz222@gmail.com>
2024-07-23 19:36:45 -0400

    Add project files.

---
+++ Controllers/LoginPageController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CampsiteReservationApp.Controllers
+{
+    public class LoginPageController : Controller
+    {
+        // Every public method in a controller is an HTTP endpoint
+        // GET: /LoginPage/
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
